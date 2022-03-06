13ca2f22d86759b79e62414252bee611921be8c2
James Bradford <j.bradford@pnnl.gov>
2022-03-06 12:34:01 -0800

    adding example js file for initNetlifyLfsImageData()

---
+++ src/example/getNetlifyLfsImage.js
@@ -0,0 +1,10 @@
+import { initNetlifyLfsImageData } from "gatsby-source-netlify-lfs";
+import imageData from "./netlifyLfsImageData.json";
+
+/** default IGetNetlifyLfsImageDataArgs */
+const defaultArgs = {
+    backgroundColor: 'hsl(0deg 0% 1%)',
+};
+
+/** use in place of getImage() from "gatsby-plugin-image" */
+export const getNetlifyLfsImage = initNetlifyLfsImageData(imageData, defaultArgs);
